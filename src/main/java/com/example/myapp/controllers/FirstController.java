36f8ba4aa62f2173515ad3e69cd9db7c9adf3e37
MARVINAKI <kosakbig@gmial.com>
2022-12-30 19:56:19 +0500

    for check

--- src/main/java/com/example/myapp/controllers/FirstController.java
+++ src/main/java/com/example/myapp/controllers/FirstController.java
@@ -3,11 +3,25 @@ package com.example.myapp.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 
 @RestController
 public class FirstController {
     @GetMapping
     public String greetings() {
         return "Приложение запущено";
+    }
+
+    @GetMapping
+    public String infoPage() {
+        String nameOfPupil = "Konstantin";
+        String nameOfProject = "MyApp";
+        String description = "Приложения для сайта рецептов";
+        LocalDate timeCreation = LocalDate.now();
+        return "Имя ученика: " + nameOfPupil + ";\n"
+                + "Название приложения: " + nameOfProject + ";\n"
+                + "Дата создания проекта: " + timeCreation + ";\n"
+                + "Описание проекта: " + description;
     }
 }
